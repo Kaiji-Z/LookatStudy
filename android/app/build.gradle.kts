87db35dfb74b9fa0b08831cbb4bde130c6805ab8
Kaiji-Z <Kaiji-Z@users.noreply.github.com>
2026-08-18 19:57:43 +0800

    chore(release): v0.11.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lookatstudy.launcher"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.10.0"
+        versionCode = 3
+        versionName = "0.11.0"
     }
 
     aaptOptions {
